003102a340a7cdd1453f22bd5ed4539944e1bc10
susheelsahoo <susheelcs24@gmail.com>
2025-03-08 14:53:13 +0400

    setup lead

--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -18,9 +18,9 @@ class AdminSeeder extends Seeder
         if (is_null($admin)) {
             $admin           = new Admin();
             $admin->name     = "Super Admin";
-            $admin->email    = "superadmin@example.com";
-            $admin->username = "superadmin";
-            $admin->password = Hash::make('12345678');
+            $admin->email    = "susheelcs24@gmail.com";
+            $admin->username = "susheel.sahoo";
+            $admin->password = Hash::make('Ankit@143@');
             $admin->save();
         }
     }
